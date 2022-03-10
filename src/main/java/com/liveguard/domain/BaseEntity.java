9f5e1afca68d02ea033ff335a487839fe30bd51a
Mohamed Hashad <mohamed085555@gmail.com>
2022-03-10 14:06:32 +0200

    Create country and state entity

--- src/main/java/com/liveguard/domain/BaseEntity.java
+++ src/main/java/com/liveguard/domain/BaseEntity.java
@@ -18,6 +18,8 @@ import java.io.Serializable;
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
---
+++ src/main/java/com/liveguard/domain/State.java
@@ -0,0 +1,50 @@
+package com.liveguard.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "states")
+@NoArgsConstructor
+@Getter
+@Setter
+public class State extends BaseEntity {
+
+    @Column(nullable = false, length = 45)
+    private String name;
+
+    @JsonManagedReference
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    public State(String name, Country country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "State [id=" + super.getId() + ", name=" + name + "]";
+    }
+
+
+}
